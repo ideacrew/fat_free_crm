b5821367d60861038ae6dc57292fe292260dd5a3
zasman <zanesandin@gmail.com>
2020-06-12 15:50:17 -0400

    Pushes index specs controllers forward

--- spec/controllers/fat_free_crm/index_cases_controller_spec.rb
+++ spec/controllers/fat_free_crm/index_cases_controller_spec.rb
@@ -6,35 +6,45 @@ module FatFreeCrm
   describe IndexCasesController do
     routes { FatFreeCrm::Engine.routes }
     let(:account) { create(:account, user: current_user) }
+    let(:index_case) {create(:index_case, user: current_user)}
 
-    before(:each) do
+    before do
       login
+      index_case
       account
       set_current_tab(:index_cases)
     end
 
     describe "responding to GET index" do
       it "should expose all index cases as @contacts and render [index] template" do
-        @index_cases = [create(:index_case, user: current_user)]
-        get :index
+         @index_case_category_total = {
+          total: 1,
+          other: index_case
+        }
+        # allow(controller).to receive(:get_data_for_sidebar).and_return(@index_case_category_total)
+        @index_cases = [index_case]
+        get :index, xhr: true
+        # expect(response[:index_cases]).to eq(@index_cases)
         #expect(assigns[:index_cases].count).to eq(@index_cases.count)
         #expect(assigns[:index_cases]).to eq(@index_cases)
         # expect(response).to render_template("index_cases/index")
-        expect(response).to redirect_to("/fat_free_crm/index_cases")
+        expect(response.status).to eq(200)
+        # expect(response).to render_template("/fat_free_crm/index_cases")
       end
     end
 
     describe "responding to GET show" do
       before(:each) do
-        #@stage = Setting.unroll(:opportunity_stage)
-        #@comment = Comment.new
-        #@timeline = [] #timeline(@index_case)
-        @index_case = create(:index_case, user: current_user)
+        @stage = Setting.unroll(:opportunity_stage)
+        @comment = Comment.new
+        @timeline = [] #timeline(@index_case)
+        @index_case = index_case
       end
 
       it "should expose the requested index case as @index_case" do
-        get :show, params: {id: @index_case.id}
-        expect(response).to render_template("")
+        get :show, params: {id: @index_case.id}, xhr: true
+        expect(response.status).to eq(200)
+        # expect(response).to render_template("/fat_free_crm/index_case")
       end
     end
   end
