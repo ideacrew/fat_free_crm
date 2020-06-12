06fa2db4ae001362b6e19c4fcbce384594b9799c
Trey Evans <tevans@justplainawful.lan>
2020-06-12 11:28:27 -0400

    Update level migrations.

--- db/migrate/20200531112010_create_levels.rb
+++ db/migrate/20200531112010_create_levels.rb
@@ -2,7 +2,7 @@ class CreateLevels < ActiveRecord::Migration[6.0]
   def change
     create_table :fat_free_crm_levels do |t|
       t.string :name
-      t.references :facility
+      t.references :facility, {:foreign_key => {:to_table => :fat_free_crm_facilities}}
 
       t.timestamps
     end
