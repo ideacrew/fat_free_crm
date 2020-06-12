b6a6e7bdfc8d782dc0bf6a6dce98b67e381690d0
Bill Transue <transue@gmail.com>
2020-06-12 10:22:54 -0400

    Remove unneeded option

--- db/migrate/20200611133345_add_level_id_to_assignments.rb
+++ db/migrate/20200611133345_add_level_id_to_assignments.rb
@@ -1,5 +1,4 @@
 class AddLevelIdToAssignments < ActiveRecord::Migration[6.0]
   def change
-    add_reference :fat_free_crm_assignments, :level, foreign_key: true
-  end
+    add_reference :fat_free_crm_assignments, :level
 end
