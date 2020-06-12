be9e795a00ee09e6e41bd88c8bab4514279ce0cd
zasman <zanesandin@gmail.com>
2020-06-12 13:39:55 -0400

    REFS Index Case specs - Adds index case spec and replaces variables with entity

--- spec/views/fat_free_crm/index_cases/_new.html.haml_spec.rb
+++ spec/views/fat_free_crm/index_cases/_new.html.haml_spec.rb
@@ -2,15 +2,26 @@ require 'spec_helper'
 
 module FatFreeCrm
   describe "/fat_free_crm/index_cases/_new" do
+    let(:contact) { create(:contact) }
+    let(:index_case) { create(:index_case, contact: contact) }
 
     before do
       login
-      assign(:index_case, create(:index_case))
+      assign(:index_case, index_case)
+    end
 
     it "should render [create index_case] form" do
       render
-      expect(view).to render_template(partial: "fat_free_crm/index_cases/_top_section")
-      expect(view).to render_template(partial: "fat_free_crm/index_cases/_extra")
+      # Form
+      expect(rendered).to include('<form class="edit_index_case"')
+      # Top Section
+      expect(rendered).to include("<label>Phone:</label>")
+      expect(rendered).to include("<label>Email:</label>")
+      expect(rendered).to include("<label>Window Start Date:</label>")
+      expect(rendered).to include("<label>Window End Date:</label>")
+      expect(rendered).to include("<label>Projected Return Date:</label>")
+      expect(rendered).to include("<summary>Permissions</summary>")
+      expect(rendered).to include('<label for="index_case_group_ids">Groups:</label>')
     end
   end
 end
