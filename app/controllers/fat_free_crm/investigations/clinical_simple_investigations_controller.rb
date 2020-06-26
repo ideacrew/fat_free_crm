e2cf1e7d24343ad869a6c5f2cd650973edab53d9
raghuramg <raghuramg83@gmail.com>
2020-06-26 11:17:30 -0400

    Added error message redirection

--- app/controllers/fat_free_crm/investigations/clinical_simple_investigations_controller.rb
+++ app/controllers/fat_free_crm/investigations/clinical_simple_investigations_controller.rb
@@ -6,8 +6,12 @@ module FatFreeCrm
 
       def generate_osha
         osha_form = IndexCases::Osha::Form301::Serialize.new.call(investigation: @clinical_simple_investigation)
-
-        send_file osha_form.success, filename: "osha_301.xlsx", disposition: 'attachment'
+       
+        if osha_form.success?
+          send_file osha_form.success, filename: "osha_301.xlsx", disposition: 'attachment'
+        else
+          redirect_to index_cases_path, flash: { error: "OSHA-301 generation failed due to #{osha_form.failure.errors.to_h}" }
+        end
       end
     end
   end
