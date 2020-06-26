470bbc3625f11d0a2b2f5a787c1e29dbb0b6bb52
harshaellanki <h_vardhan@yahoo.com>
2020-06-26 13:50:00 -0400

    deleted exposureobserver

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,6 @@ module FatFreeCrm
       :"FatFreeCrm::OpportunityObserver",
       :"FatFreeCrm::TaskObserver",
       :"FatFreeCrm::IndexCaseObserver",
-      :"FatFreeCrm::ExposureObserver",
       :"FatFreeCrm::EntityObserver"
       # :lead_observer, :opportunity_observer, :task_observer, :entity_observer
     ] unless ARGV.join.include?('assets:precompile')
--- lib/fat_free_crm/engine.rb
+++ lib/fat_free_crm/engine.rb
@@ -71,7 +71,6 @@ module FatFreeCrm
       "FatFreeCrm::OpportunityObserver",
       "FatFreeCrm::TaskObserver",
       "FatFreeCrm::IndexCaseObserver",
-      "FatFreeCrm::ExposureObserver",
       "FatFreeCrm::EntityObserver"
       # :lead_observer, :opportunity_observer, :task_observer, :entity_observer
     ]
