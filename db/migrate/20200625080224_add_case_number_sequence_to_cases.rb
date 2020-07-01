0f6de243a19ffd6b03fdb6ac1406ffba55b14872
raghuramg <raghuramg83@gmail.com>
2020-07-01 19:57:08 -0400

    Fixed add fields button js to load contact fields properly

---
+++ db/migrate/20200625080224_add_case_number_sequence_to_cases.rb
@@ -0,0 +1,19 @@
+class AddCaseNumberSequenceToCases < ActiveRecord::Migration[4.2]
+  def self.up
+    execute <<-SQL
+      CREATE SEQUENCE case_number_seq INCREMENT 1 START 1000;
+      ALTER TABLE fat_free_crm_index_cases ALTER COLUMN case_number SET DEFAULT nextval('case_number_seq');
+      ALTER TABLE fat_free_crm_exposure_cases ALTER COLUMN case_number SET DEFAULT nextval('case_number_seq');
+      ALTER TABLE fat_free_crm_facility_cases ALTER COLUMN case_number SET DEFAULT nextval('case_number_seq');
+    SQL
+  end
+
+  def self.down
+    execute <<-SQL
+      ALTER TABLE fat_free_crm_index_cases ALTER COLUMN case_number SET NOT NULL;
+      ALTER TABLE fat_free_crm_exposure_cases ALTER COLUMN case_number SET NOT NULL;
+      ALTER TABLE fat_free_crm_facility_cases ALTER COLUMN case_number SET NOT NULL;
+      DROP SEQUENCE case_number_seq;
+    SQL
+  end
+end
