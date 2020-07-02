26ad7822dbd3404cbb5446257efb7a4a07c4247a
harshaellanki <h_vardhan@yahoo.com>
2020-07-01 20:25:40 -0400

    removed duplicate migration

--- db/migrate/20200630160815_add_exposure_level_to_contact_simple_exposure.rb
+++
@@ -1,5 +0,0 @@
-class AddExposureLevelToContactSimpleExposure < ActiveRecord::Migration[6.0]
-  def change
-    add_column :fat_free_crm_exposures_contact_simple_exposures, :exposure_level, :string
-  end
-end
