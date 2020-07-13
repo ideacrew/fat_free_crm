f8f1ffe35b2f4371eb842a68c78cd02f4c6a5390
harshaellanki <h_vardhan@yahoo.com>
2020-07-13 13:09:06 -0400

    added address_observer to add lonlat to the address before saving

---
+++ app/models/fat_free_crm/address_observer.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module FatFreeCrm
+  class AddressObserver < ActiveRecord::Observer
+    observe :"::FatFreeCrm::Address"
+
+    def before_save(item)
+      set_lonlat_to_address(item)
+    end
+
+    private
+
+    def set_lonlat_to_address(address)
+      return unless address.valid?
+      result = Google::Maps::GeocodeAddress.new.call(key: CovidMostRegistry[:google_api].setting(:google_api_key).item, address: address_into_string(address))
+
+      if result.success?
+        address.full_address = result.value!.to_h[:results].first[:formatted_address]
+        lonlat_hash = result.value!.to_h[:results].first[:geometry][:location]
+        lonlat_point = create_point(lonlat_hash["lat"], lonlat_hash["lng"] )
+        address.lonlat = lonlat_point
+      end
+    end
+
+    def address_into_string(address)
+      address[:street1] + ", " + address[:street2] + ", " +
+      address[:city] + ", " + address[:state] + ", " +
+      address[:country] + ", " + address[:zipcode]
+    end
+
+    def create_point(lat, lng)
+      point = Maps::PostGis::CreatePointShape.new.call(:lat => lat, :lng => lng)
+      point.success? ? point.value! : nil
+    end
+
+    ActiveSupport.run_load_hooks(:AddressObserver, self)
+  end
+end
+
