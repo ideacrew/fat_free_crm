4fc0bce675f3d6dffa1653832604acc345fe45d5
harshaellanki <h_vardhan@yahoo.com>
2020-09-09 16:03:38 -0400

    added scope to wellness status

--- app/models/fat_free_crm/wellness_status.rb
+++ app/models/fat_free_crm/wellness_status.rb
@@ -11,5 +11,6 @@ module FatFreeCrm
     enum response_states: {no_response: 'no_response', response_submitted: 'response_submitted', response_not_required: 'response_not_required', absent_index_case_status: 'absent_index_case_status', absent_exposure_case_status: 'absent_exposure_case_status', absent_pto_status: 'absent_pto_status', absent_remote_status: 'absent_remote_status', absent_not_scheduled: 'absent_not_scheduled', absent_other: 'absent_other'}
     default_scope { order(created_at: :asc) }
 
+    scope :current, -> { where(attested_at: Date.today.all_day) }
   end
 end
